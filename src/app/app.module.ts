a20c11c0c7b9d35e76970e47ec68be8fb47b8689
kaiquemf <kaiquemartinsdasilvaferreira@gmail.com>
2023-06-14 17:14:46 -0300

    add: criação formulário adicionar paciente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,17 +8,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ListaPacientesComponent } from './components/lista-pacientes/lista-pacientes.component';
+import { AdicionarpacienteComponent } from './components/adicionar-paciente/adicionarpaciente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListaPacientesComponent
+    ListaPacientesComponent,
+    AdicionarpacienteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     FontAwesomeModule
   ],
   providers: [],
